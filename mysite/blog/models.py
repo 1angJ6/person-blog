3c0713ef3b382fec1eacc9b42d33da5f2157ec08
1angJ6 <358526394@qq.com>
2017-07-28 17:29:12 +0800

    Add "Article" models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    tag = models.CharField(max_length=20)
+    last_update_date = models.DateField
+    text = models.TextField
